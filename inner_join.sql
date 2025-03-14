f374f60b73949ffb3866d6564f675b5a611f6b69
Fernando Bordin <fernandobordin07@hotmail.com>
2025-03-13 21:56:42 -0300

    Corrigindo ao adicionar tabela com chave estrangeira e realizando relacionamento fk

--- inner_join.sql
+++ inner_join.sql
@@ -1,14 +1,34 @@
--- Relacionamentos entre tabelas
--- #############################
+-- Relacionamento entre tabelas
+--#############################
 
--- Alterando nome da coluna "id" tabela "product", para condizer com a tabela "stock"
-ALTER TABLE product RENAME COLUMN id TO id_product
+-- Deletando tabelas existentes
+DROP TABLE product;
 
--- Inserindo registro na tabela de produto
-INSERT INTO product (id_product, name, value) VALUES (2, 'Nintendo Switch', '1950.00');
+DROP TABLE stock;
 
--- Inserindo registro na tabela stock
-INSERT INTO stock (id_stock, id_product, date, obs) VALUES (2, 2, '2025-03-13', 'Versão Mario Kart 8 Deluxe');
+-- Criando tabela produto com chave primária no id
+CREATE TABLE product (id INTEGER PRIMARY KEY, name VARCHAR(30), value VARCHAR(30));
+
+-- Criando tabela com chave estrangeira
+CREATE TABLE stock (id INTEGER PRIMARY KEY, id_product INTEGER REFERENCES product(id));
+
+-- Adicionando relacionamento(FK) entre tabelas
+CREATE TABLE stock (id INTEGER PRIMARY KEY,
+                        id_product INTEGER,
+                        date DATE NOT NULL,
+                        obs VARCHAR(100) NOT NULL,
+                        CONSTRAINT fk_id_product FOREIGN KEY (id_product) REFERENCES product (id)
+                    );
+
+-- Inserindo dados na tabela Produto
+INSERT INTO product (id, name, value) VALUES (1, 'Xbox Series X', '3650.00');
+
+INSERT INTO product (id, name, value) VALUES (2, 'PlayStation 5', '3350.00');
+
+-- Inserindo dados na tabela Estoque
+INSERT INTO stock (id, id_product, date, obs) VALUES (1, 1, '2025-03-13', 'Video Game Especial Edition');
+
+INSERT INTO stock (id, id_product, date, obs) VALUES (2, 2, '2025-03-13', 'Video Game Portatil');
 
 -- Relacionamento entre tabelas unificado
 SELECT  product.name,
@@ -16,7 +36,7 @@ SELECT  product.name,
         date,
         obs
 FROM product
-INNER JOIN stock ON product.id_product = stock.id_product;
+INNER JOIN stock ON product.id = stock.id_product;
 
 -- Relacionamento entre tabelas com mais filtros
 SELECT  product.name,
@@ -24,6 +44,6 @@ SELECT  product.name,
         date,
         obs
 FROM product
-INNER JOIN stock ON product.id_product = stock.id_product; AND product.name = 'Nintendo Switch';
+INNER JOIN stock ON product.id = stock.id_product; AND product.name = 'PlayStation 5';
 
 
